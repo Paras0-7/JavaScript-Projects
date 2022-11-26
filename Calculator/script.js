13c1b7aaf6d16cb723553cb0658c1e12967838a8
Paras0-7 <parasrawat937@gmail.com>
2022-11-26 19:40:40 +0530

    reactJS

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,30 +1,62 @@
-const btns = document.querySelectorAll(".btn");
-const output = document.querySelector(".output");
+const memoise = function (fn) {
+  let cahce = {};
 
-btns.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    let op;
-    if (e.target.value === "DEL") {
-      op = output.value.trim().split(" ");
-      op.pop();
-      op = op.join(" ");
-      output.value = "";
-      console.log(op);
+  return function (...args) {
+    const argsString = JSON.stringify(args);
 
-      showOp(op);
-    } else if (e.target.value === "=") {
-      op = output.value;
-    } else if (e.target.value === "Ans") {
-      console.log("PAras");
-    } else if (e.target.value == "AC") {
-      output.value = "";
-    } else showOp(e.target.value);
-  });
+    if (cahce[argsString]) {
+      console.log("Fetching");
+    } else {
+      console.log("Calculating");
+      cahce[argsString] = fn.apply(null, args);
+    }
+
+    return cahce[argsString];
+  };
+};
+
+const add = function (a, b, c) {
+  return a + b + c;
+};
+
+// const sum = memoise(add);
+// console.log(sum(10, 10, 10));
+// console.log(sum(10, 10, 10));
+// console.log(sum(10, 10, 10));
+
+const Factorial = memoise(function (x) {
+  if (x == 1) return 1;
+  return x * Factorial(x - 1);
 });
 
-const showOp = function (value) {
-  let op = output.value;
-  op += `${value}`;
+// console.log(Factorial(5));
+// console.log(Factorial(4));
 
-  output.value = op;
+const sum = function (a) {
+  return function (b) {
+    if (b) return sum(a + b);
+    return a;
+  };
 };
+
+console.log(sum(1)(2)(3)(4)());
+
+(function par() {
+  console.log("Paras");
+})();
+
+const arr = [1, 2, [3, 4, [5, 6]], [7, 8]];
+
+const flat = function (arr, level = 1) {
+  const newArr = arr.reduce(function (acc, ele) {
+    if (Array.isArray(ele) && level > 0) {
+      acc = acc.concat(flat(ele, level - 1));
+    } else acc.push(ele);
+
+    return acc;
+  }, []);
+
+  return newArr;
+};
+
+console.log(flat(arr, 2));
